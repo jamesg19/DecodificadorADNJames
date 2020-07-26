9deb2938d2c6069678b1a903a29c4dbcba85a413
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2020-07-26 17:07:37 -0400

    pide datos

--- DecodificadorADNJames/src/main/java/jamescunoc/decodificadoradnjames/Decodificador.java
+++ DecodificadorADNJames/src/main/java/jamescunoc/decodificadoradnjames/Decodificador.java
@@ -5,14 +5,36 @@
  */
 package jamescunoc.decodificadoradnjames;
 
+import java.util.Scanner;
+
 /**
  *
  * @author james
  */
 public class Decodificador {
+    Scanner entrada=new Scanner(System.in);
+    public String SecuenciaADN1;
+    public String SecuenciaADN2;
+    int CantidadLetrasADN1;
+    int CantidadLetrasADN2;
+    
     
     
     public void pedirSecuencia1(){
-        
+        System.out.println("Ingresa la secuencia de ADN 1:");
+        SecuenciaADN1=entrada.nextLine();
+        pedirSecuencia2();
     }
+    
+    public void pedirSecuencia2(){
+        System.out.println("Ingresa la secuencia de ADN 1:");
+        SecuenciaADN2=entrada.nextLine();
+    }
+    
+    public void Proceso(){
+        CantidadLetrasADN1=SecuenciaADN1.length();
+        CantidadLetrasADN2=SecuenciaADN2.length();
+    }
+    
+    
 }
